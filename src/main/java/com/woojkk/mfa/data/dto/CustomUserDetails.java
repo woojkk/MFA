7e597b3b38c6f26788e1b765ffe189c6fd81fbc5
woojin Kim <woojkk@naver.com>
2023-10-27 16:08:04 +0900

    [Feat] - CustomUserDetails : Spring Security의 UserDetails를 구현한 사용자 정보 클래스 생성

---
+++ src/main/java/com/woojkk/mfa/data/dto/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package com.woojkk.mfa.data.dto;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@ToString
+@NoArgsConstructor
+public class CustomUserDetails implements UserDetails {
+  private String username;
+  private String password;
+  private List<? extends GrantedAuthority> authorities;
+  private boolean enabled;
+  private boolean accountNonExpired;
+  private boolean accountNonLocked;
+  private boolean credentialsNonExpired;
+
+
+  @Builder
+  public CustomUserDetails(String username, String password,
+      List<? extends GrantedAuthority> authorities, boolean enabled, boolean accountNonExpired,
+      boolean accountNonLocked, boolean credentialsNonExpired) {
+    this.username = username;
+    this.password = password;
+    this.authorities = authorities;
+    this.enabled = enabled;
+    this.accountNonExpired = accountNonExpired;
+    this.accountNonLocked = accountNonLocked;
+    this.credentialsNonExpired = credentialsNonExpired;
+  }
+}
